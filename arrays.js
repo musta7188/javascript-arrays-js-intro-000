cbe3b127dc9dedb9d6a3a69f536e9eec56f22d18
favorable-principle-2048 <musta7188@hotmail.it>
2019-11-22 21:34:28 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -4,8 +4,8 @@ var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 var Myarray1 = []
 
 function addElementToBeginningOfArray(array, element){
-  Myarray1.unshift()
-  return Myarray1;  
+  array.unshift(element)
+  return array  
 }
 
 
