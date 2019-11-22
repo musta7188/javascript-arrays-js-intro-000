43fb1b7db5d72c27b8106c4d240d57b3c6f34732
favorable-principle-2048 <musta7188@hotmail.it>
2019-11-22 21:56:17 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -21,7 +21,7 @@ function addElementToEndOfArray(array, element){
 
 function destructivelyAddElementToEndOfArray(array, element){
 
-  array.shift(element)
+  array.push(element)
   return array
 }
 
