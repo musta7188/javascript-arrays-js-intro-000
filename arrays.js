5aa53288c900d1dc99a9852c97fb68c83c2e5439
favorable-principle-2048 <musta7188@hotmail.it>
2019-11-22 23:00:29 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -60,9 +60,7 @@ destructivelyRemoveElementFromEndOfArray(1, 2, 3)
 
 function removeElementFromEndOfArray(array){
 
- array.slice(0, array.length -1)
-
-return array
+return array.slice(0, array.length -1)
 
 }
 
